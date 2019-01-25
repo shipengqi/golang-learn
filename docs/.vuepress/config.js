8cb8eaedadead0cebac0004f073a501ada208697
shipengqi <pooky.shipengqi@gmail.com>
2019-01-25 18:07:00 +0800

    type convert

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     markdown: {
         toc: {
-            includeLevel: [2, 3, 4, 5, 6]
+            includeLevel: [2, 3, 4, 5, 6, 7]
         }
     },
     themeConfig: {
